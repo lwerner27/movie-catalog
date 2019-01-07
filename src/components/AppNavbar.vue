f1bb852cc000e47d4c306874f7d0004828fa1660
lwerner27 <lwerner27@gmail.com>
2019-01-07 14:56:26 -0600

    changed some styles on the navbar

--- src/components/AppNavbar.vue
+++ src/components/AppNavbar.vue
@@ -1,12 +1,12 @@
 <template>
     <div>
         <nav>
-            <div class="nav-wrapper">
+            <div class="nav-wrapper container">
                 <a href="#!" class="brand-logo">Logo</a>
                 <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="#"><i class="large material-icons">person</i></a></li>
                     <li><a href="#"><i class="large material-icons">cloud_upload</i></a></li>
+                    <li><a href="#"><i class="large material-icons">person</i></a></li>
                 </ul>
             </div>
         </nav>
