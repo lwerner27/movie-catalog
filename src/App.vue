860fa3276d4d5a1341b8bbbeb9dbb8ef14047075
lwerner27 <lwerner27@gmail.com>
2019-01-07 14:55:06 -0600

    added navbar and basic links

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  <div>
+    <app-navbar></app-navbar>
   </div>
 </template>
 
-// <script>
-// import HelloWorld from './components/HelloWorld.vue'
+<script>
+import AppNavbar from './components/AppNavbar.vue'
 
-// export default {
-//   name: 'app',
-//   components: {
-//     HelloWorld
-//   }
-// }
+export default {
+  name: 'app',
+  components: {
+    AppNavbar
+  }
+}
 </script>
 
 <style>
---
+++ src/components/AppNavbar.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <nav>
+            <div class="nav-wrapper">
+                <a href="#!" class="brand-logo">Logo</a>
+                <a href="#" data-target="mobile-demo" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                <ul class="right hide-on-med-and-down">
+                    <li><a href="#"><i class="large material-icons">person</i></a></li>
+                    <li><a href="#"><i class="large material-icons">cloud_upload</i></a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <ul class="sidenav" id="mobile-demo">
+            <li><a href="#"><i class="small material-icons">person</i>Login</a></li>
+            <li><a href="#"><i class="small material-icons">cloud_upload</i>Upload</a></li>
+        </ul>
+    </div>
+</template>
