8bf8dfbacb6abf1719301d47f5314cffef8b5800
lwerner27 <lwerner27@gmail.com>
2019-01-10 15:54:37 -0600

    added the basics for the AddMovie page

---
+++ src/pages/AddMovie.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="add-movie-page">
+        <div class="container my-container">
+
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    data: () => (
+        {
+            movieTitle: "",
+            movieStars: "",
+            imdbLink: "",
+            movieImg: "",
+            movieDescription: ""
+        }
+    )
+}
+</script>
+
+<style scoped>
+    .my-container {
+        padding-top: 50px;
+    }
+</style>
+
