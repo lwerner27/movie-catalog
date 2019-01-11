ba356d89a893c0a66d6041a0a460a69c2dbe0379
lwerner27 <lwerner27@gmail.com>
2019-01-11 10:28:04 -0600

    updated the form layout

--- src/pages/AddMovie.vue
+++ src/pages/AddMovie.vue
@@ -1,65 +1,71 @@
 <template>
     <div class="add-movie-page">
         <div class="container my-container">
+            <!-- New Row -->
             <div class="row">
                 <div class="col s12 m6 offset-m3">
                     <h3>Add A Movie</h3>
                     <hr>
                 </div>
             </div>
+            <!-- New Row -->
             <div class="row">
                 <div class="input-field col s12 m6 offset-m3">
                     <input id="title" type="text" class="validate">
                     <label for="title">Title</label>
                 </div>
             </div>
+            <!-- New Row -->
             <div class="row">
                 <div class="input-field col s12 m6 offset-m3">
                     <input id="stars" type="text" class="validate">
                     <label for="stars">Stars</label>
                     <span class="helper-text">Separate names with a comma.</span>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12 m6 offset-m3">
-                        <input id="imdbLink" type="text" class="validate">
-                        <label for="imdbLink">IMDb Link</label>
-                    </div>
+            </div>
+            <!-- New Row -->
+            <div class="row">
+                <div class="input-field col s12 m3 offset-m3">
+                    <input id="imdbLink" type="text" class="validate">
+                    <label for="imdbLink">IMDb Link</label>
                 </div>
-                <div class="row">
-                    <div class="input-field col s12 m6 offset-m3">
-                        <input id="imgLink" type="text" class="validate">
-                        <label for="imgLink">Image Link</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="input-field col s12 m6 offset-m3">
-                        <textarea id="description" class="materialize-textarea"></textarea>
-                        <label for="textarea1">Description</label>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col s12 m6 offset-m3">
-                        <form action="#">
-                            <div class="file-field input-field">
-                                <div class="btn">
-                                    <span>File</span>
-                                    <input type="file">
-                                </div>
-                                <div class="file-path-wrapper">
-                                    <input class="file-path validate" type="text">
-                                </div>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-                <div class="row">
-                    <div class="col 12 m6 offset-m3">
-                        <button class="btn waves-effect waves-light" type="submit" name="action">Submit
-                            <i class="material-icons right">send</i>
-                        </button>
-                    </div>
+                <div class="input-field col s12 m3">
+                    <input id="imgLink" type="text" class="validate">
+                    <label for="imgLink">Image Link</label>
                 </div>
             </div>
+            <!-- New Row -->
+            <div class="row">
+                <div class="input-field col s12 m6 offset-m3">
+                    <textarea id="description" class="materialize-textarea"></textarea>
+                    <label for="textarea1">Description</label>
+                </div>
+            </div>
+            <!-- New Row -->
+            <div class="row">
+                <div class="col s12 m6 offset-m3">
+                    <form action="#">
+                        <div class="file-field input-field">
+                            <div class="btn">
+                                <span>File</span>
+                                <input type="file">
+                            </div>
+                            <div class="file-path-wrapper">
+                                <input class="file-path validate" type="text">
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+            <!-- New Row -->
+            <div class="row">
+                <div class="col 12 m6 offset-m3">
+                    <button class="btn waves-effect waves-light" type="submit" name="action">Submit
+                        <i class="material-icons right">send</i>
+                    </button>
+                </div>
+            </div>
+            <!-- End Of Rows -->
         </div>
     </div>
 </template>
